f801af3415f3efe526f3558638bce776cc6bf7b4
Joshua Alvarado <joshalva3@gmail.com>
2022-12-22 15:24:20 -0600

    my or, my and done

--- index.js
+++ index.js
@@ -117,3 +117,98 @@
 //   });
 
 // 03 my or my and
+
+// function myOr(param1, param2, param3) {
+    // first thought 
+    // if(param1 === true && param2 || param3 === false)
+    // not optimizied at all ran in to trouble with remaining 
+    // do more research on !! operator
+//     if(!!param1){ //if truthy, return!
+//       return param1;
+//     }
+    
+//     if(!!param2){
+//       return param2;
+//     }
+    
+//     if(!!param3){
+//       return param3;
+//     }
+    
+//     return param3; //else, all of the values are falsey. return the last value
+//   }
+  
+//   function myAnd(param1, param2, param3) {
+//     if(!param1){
+//       return param1;
+//     }
+    
+//     if(!param2){
+//       return param2;
+//     }
+    
+//     if(!param3){
+//       return param3;
+//     }
+    
+//     return param3;
+//   }
+  
+//   describe('myOr', () => {
+  
+//     it('is a function', () => {
+//       expect(typeof myOr).toEqual('function');
+//     });
+  
+//     it('returns the first value if it is the first truthy value', () => {
+//       let returnedValue = myOr('apples', 1, false);
+//       expect(returnedValue).toEqual('apples');
+//     });
+  
+//     it('returns the second value if it is the first truthy value', () => {
+//       let returnedValue = myOr(null, 'i am truthy', 'also true');
+//       expect(returnedValue).toEqual('i am truthy');
+//     });
+  
+//     it('returns the third value if it is the first truthy value', () => {
+//       let returnedValue = myOr(false, false, true);
+//       expect(returnedValue).toEqual(true);
+//     });
+  
+//     it('returns the third value if none of the values are truthy', () => {
+//       let returnedValue = myOr('', 0, null);
+//       expect(returnedValue).toEqual(null);
+//     });
+  
+//   });
+  
+//   describe('myAnd', () => {
+  
+//     it('is a function', () => {
+//       expect(typeof myAnd).toEqual('function');
+//     });
+  
+//     it('returns the first value if it is the first falsey value', () => {
+//       let returnedValue = myAnd(0, 1, false);
+//       expect(returnedValue).toEqual(0);
+//     });
+  
+//     it('returns the second value if it is the first falsey value', () => {
+//       let returnedValue = myAnd(20, '', 'i am truthy');
+//       expect(returnedValue).toEqual('');
+//     });
+  
+//     it('returns the third value if it is the first falsey value', () => {
+//       let returnedValue = myAnd(true, 'happy', 0);
+//       expect(returnedValue).toEqual(0);
+//     });
+  
+//     it('returns the third value if all of the values are truthy', () => {
+//       let returnedValue = myAnd('finally', 'we did it!', 'yay!');
+//       expect(returnedValue).toEqual('yay!');
+//     });
+  
+//   });
+
+// 04 Only One
+
