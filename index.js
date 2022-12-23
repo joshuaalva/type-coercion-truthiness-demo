fcf5ecb4101d787aa3a175aa1231c3a4b78d5db8
Joshua Alvarado <joshalva3@gmail.com>
2022-12-23 14:35:35 -0600

    only one workshop problem one problem left

--- index.js
+++ index.js
@@ -212,3 +212,73 @@
 
 // 04 Only One
 
+// function onlyOne(param1, param2, param3){
+//     // if only the first value is truthy return true;
+//     if(!!param1 && !param2 && !param3){
+//       return true;
+//     }
+//     // if only the second value is truthy return true;
+//     if(!param1 && !!param2 && !param3){
+//       return true;
+//     }
+//     // if only the third value is truthy
+//     if(!param1 && !param2 && !!param3){
+//       return true;
+//     }
+    
+//     return false;
+//   }
+  
+//   describe('onlyOne', () => {
+  
+//     it('is a function', () => {
+//       expect(typeof onlyOne).toEqual('function');
+//     });
+  
+//     it('returns a boolean', () => {
+//       let returnedValue = onlyOne(1, 1, 1);
+//       expect(typeof returnedValue).toEqual('boolean');
+//     });
+  
+//     it('returns true if only the first value is truthy', () => {
+//       let returnedValue = onlyOne(1, 0, false);
+//       expect(returnedValue).toEqual(true);
+//     });
+  
+//     it('returns true if only the second value is truthy', () => {
+//       let returnedValue = onlyOne(null, 'hi!', undefined);
+//       expect(returnedValue).toEqual(true);
+//     });
+  
+//     it('returns true if only the third value is truthy', () => {
+//       let returnedValue = onlyOne(0, '', true);
+//       expect(returnedValue).toEqual(true);
+//     });
+  
+//     it('returns false if all of the values are falsey', () => {
+//       let returnedValue = onlyOne(false, null, undefined);
+//       expect(returnedValue).toEqual(false);
+//     });
+  
+//     it('returns false if only the first value is falsey', () => {
+//       let returnedValue = onlyOne('', -20, true);
+//       expect(returnedValue).toEqual(false);
+//     });
+  
+//     it('returns false if only the second value is falsey', () => {
+//       let returnedValue = onlyOne(true, null, 'i am truthy');
+//       expect(returnedValue).toEqual(false);
+//     });
+  
+//     it('returns false if only the third value is falsey', () => {
+//       let returnedValue = onlyOne(true, 'pumpkin', 0);
+//       expect(returnedValue).toEqual(false);
+//     });
+  
+    it('returns false if all of the values are truthy', () => {
+      let returnedValue = onlyOne(true, 'yes!', 100);
+      expect(returnedValue).toEqual(false);
+    });
+  
+  });
+  
